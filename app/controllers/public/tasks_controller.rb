88fe4dd8fc4eb6bd9959e555600cc0a106ffe530
Yasuaki <yasu5293koh@gmail.com>
2021-01-01 08:13:58 +0000

    user側taskのビューの骨組み

--- app/controllers/public/tasks_controller.rb
+++ app/controllers/public/tasks_controller.rb
@@ -1,2 +1,44 @@
 class Public::TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def new
+    @task = Task.new
+  end
+  
+  def index
+    @tasks = current_user.tasks
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    @task.user_id = current_user.id
+    @task.save
+    redirect_to tasks_path
+  end
+  
+  def update
+    @task.update(task_params)
+    redirect_to request.referer
+  end
+  
+  def destroy
+    @task.destroy
+    redirect_to request.referer
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :detail, :priority_status, :due, :progress_status)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  
+  def created_at_jst
+    created_at.in_time_zone('Tokyo').strftime("%Y年%m月%d日 %H:%M")
+  end
 end
