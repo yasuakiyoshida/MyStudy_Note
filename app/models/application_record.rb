3eea4f11773dbcc35b8390910ccf170ba9af7297
Yasuaki <yasu5293koh@gmail.com>
2021-01-04 22:17:08 +0900

    #28

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,21 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   
-  def created_at_jst
-    created_at.in_time_zone('Tokyo').strftime("%Y年%m月%d日 %H:%M")
+  def self.today_study_time
+    today_learnings = self.where(date: Date.today)
+    self.learning_time_sum(today_learnings)
+  end
+  
+  def self.yesterday_study_time
+    yesterday_learnings = self.where(date: Date.yesterday)
+    self.learning_time_sum(yesterday_learnings)
+  end
+  
+  def self.learning_time_sum(date_ranges)
+    total_time = 0
+    date_ranges.each do |date_range|
+      total_time += date_range.time
+    end
+    total_time.floor(1)
   end
 end
