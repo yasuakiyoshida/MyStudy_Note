8f0861ae5cfc3c1c6ca485f14b6a322e01c55c49
Yasuaki <yasu5293koh@gmail.com>
2021-01-04 05:32:41 +0000

    #22

--- app/controllers/concerns/sidebarable.rb
+++ app/controllers/concerns/sidebarable.rb
@@ -1,10 +1,8 @@
 module Sidebarable
   extend ActiveSupport::Concern
-  
   def set_sidebar
     @today_study_time = current_user.learnings.today_study_time
     @yesterday_study_time = current_user.learnings.yesterday_study_time
     @my_tasks = current_user.tasks.limit(5).order("due")
   end
-  
 end
