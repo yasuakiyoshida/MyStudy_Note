473f0ad40d462645e1060aa87c01f06c270e7b15
Yasuaki <yasu5293koh@gmail.com>
2021-01-14 14:42:45 +0900

    マイページのレイアウト、フォント変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  def set_sidebar_base_data
-    @my_today_study_time = current_user.learnings.today_study_time
-    @my_yesterday_study_time = current_user.learnings.yesterday_study_time
-    @my_tasks = current_user.tasks.limit(5).order("due")
-  end
-  
   protected
   
   def after_sign_in_path_for(resource_or_scope)
