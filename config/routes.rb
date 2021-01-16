c06d5a7c6fbadbb739ad0e76d52806e24f1c4d39
Yasuaki <yasu5293koh@gmail.com>
2021-01-16 14:49:41 +0900

    #60

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'public/sessions',
     registrations: 'public/registrations',
+    passwords: 'public/passwords'
   }
   
   scope module: :public do
