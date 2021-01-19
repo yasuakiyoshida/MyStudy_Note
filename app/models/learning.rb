25179725df474e981b25d1d2e124d2b4cdc66538
Yasuaki <yasu5293koh@gmail.com>
2021-01-19 21:32:52 +0900

    #91

--- app/models/learning.rb
+++ app/models/learning.rb
@@ -10,26 +10,27 @@ class Learning < ApplicationRecord
   validates :date, presence: true
   validates :time, presence: true, numericality: {less_than_or_equal_to: 24, greater_than: 0}
   validate :date_cannot_be_in_the_future
-  validate :total_time_cannot_exceed_24_hours ,on: :create
-  # validate :total_time_cannot_exceed_24_hours_for_edit ,on: :update
-
+  validate :total_time_cannot_exceed_limit_time ,on: :create
+  validate :total_time_cannot_exceed_limit_time_for_edit ,on: :update
+  
+  LIMIT_TIME_HOUR = 24
+  
   def one_day_time_sum(date)
-    Learning.where(date: date).sum(:time)
+    user.learnings.where(date: date).sum(:time)
   end
 
-  # learning_idがとれない
-  # def one_day_time_sum_unless_target_date(date)
-  #   Learning.where(date: date).sum(:time) - Learning.where(date: date).find_by(id: learning.id)[:time]
-  # end
+  def one_day_time_sum_unless_target_date(date)
+    user.learnings.where(date: date).where.not(id: id).sum(:time)
+  end
 
-  def total_time_cannot_exceed_24_hours
-    if date.presence && time.presence && one_day_time_sum(date) + time > 24.0
+  def total_time_cannot_exceed_limit_time
+    if date.presence && time.presence && one_day_time_sum(date) + time > LIMIT_TIME_HOUR
       errors.add(:date, "：#{date.strftime("%Y年%m月%d日")}の学習時間の合計が24時間を超えています")
     end
   end
 
-  def total_time_cannot_exceed_24_hours_for_edit
-    if date.presence && time.presence && one_day_time_sum_unless_target_date(date) + time > 24.0
+  def total_time_cannot_exceed_limit_time_for_edit
+    if date.presence && time.presence && one_day_time_sum_unless_target_date(date) + time > LIMIT_TIME_HOUR
       errors.add(:date, "：#{date.strftime("%Y年%m月%d日")}の学習時間の合計が24時間を超えています")
     end
   end
