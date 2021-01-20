52a73878f5c5d124aea9ea66c72e76a59e749aed
Yasuaki <yasu5293koh@gmail.com>
2021-01-20 22:52:26 +0900

    レイアウト微調整、リファクタリング等

--- app/helpers/public/learning_times_helper.rb
+++ app/helpers/public/learning_times_helper.rb
@@ -1,10 +1,10 @@
 module Public::LearningTimesHelper
-  def sum_times
-    @chart.values.inject(:+).floor(1) unless @chart.nil?
+  def sum_times(chart)
+    chart.values.inject(:+).floor(1) unless chart.nil?
   end
 
-  def to_minute
-    hour = sum_times * 60.0
+  def to_minute(chart)
+    hour = sum_times(chart) * 60.0
     hour.round(0)
   end
 end
