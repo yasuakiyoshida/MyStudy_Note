3115d9adfa9f91a287eef149caa24ea376b92876
Yasuaki <yasu5293koh@gmail.com>
2021-01-26 16:22:07 +0900

    ヘッダーの修正

--- spec/controllers/public/sessions_controller_spec.rb
+++ spec/controllers/public/sessions_controller_spec.rb
@@ -3,13 +3,12 @@ require "rails_helper"
 RSpec.describe Public::SessionsController, type: :controller do
   let(:user) { create(:user) }
   let(:another_user) { create(:user) }
-
+  
   describe "GET #newのテスト" do
     before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       get :new
     end
-
     it "ログインページへのリクエストは200 OKとなること" do
       expect(response.status).to eq 200
     end
@@ -17,7 +16,7 @@ RSpec.describe Public::SessionsController, type: :controller do
       expect(response).to render_template :new
     end
   end
-
+  
   # describe "POST #createのテスト" do
   #   context "入力されたパラメータが妥当な場合" do
   #     before do
@@ -32,7 +31,7 @@ RSpec.describe Public::SessionsController, type: :controller do
   #     #   expect(response).to redirect_to root_path
   #     # end
   #   end
-
+    
   #   context "入力されたパラメータが不正な場合" do
   #     before do
   #       @request.env["devise.mapping"] = Devise.mappings[:user]
