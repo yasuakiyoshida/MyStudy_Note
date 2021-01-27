ea2b9e7ee7c3b7f2f914f51c80ee3377ad7ce6b8
Yasuaki <yasu5293koh@gmail.com>
2021-01-27 16:33:52 +0900

    #129

--- app/controllers/public/learning_times_controller.rb
+++ app/controllers/public/learning_times_controller.rb
@@ -3,6 +3,7 @@ class Public::LearningTimesController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
+    @learnings = @user.learnings
     @period = params[:period]
     @chart = @user.learnings.learnings_period(@period)
   end
