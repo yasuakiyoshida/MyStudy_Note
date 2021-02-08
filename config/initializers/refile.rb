6add361e96e1aad16a3766ee1664ce28a8c65c77
Yasuaki <yasu5293koh@gmail.com>
2021-02-08 17:21:09 +0900

    fix/refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
   aws = {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION'],
-    bucket: ENV['AWS_S3_BUCKET_NAME']
+    region: 'ap-northeast-1',
+    bucket: ENV['AWS_S3_BUCKET_NAME'],
   }
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'store', **aws)
