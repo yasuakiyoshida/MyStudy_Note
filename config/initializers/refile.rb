a6827535fe4232e19ef618eb32d917c32ccf2392
Yasuaki <yasu5293koh@gmail.com>
2021-02-11 14:14:10 +0900

    開発環境でもS3に画像を保存するように修正

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,6 +1,4 @@
 require 'refile/s3'
-
-if Rails.env.production?
   aws = {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
@@ -9,4 +7,3 @@ if Rails.env.production?
   }
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'store', **aws)
-end
